ae66b7e18367cc79daf803703f48d243c81f1548
Rustem <slehder5657@gmail.com>
2025-02-03 13:43:38 +0500

    feat: create store

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css"
 import App from "./app/App"
 import { store } from "app/store"
 import { Provider } from "react-redux"
+import { rustemAge } from "rustem"
 
 const root = createRoot(document.getElementById("root") as HTMLElement)
 root.render(
@@ -11,3 +12,9 @@ root.render(
     <App />
   </Provider>,
 )
+
+if (process.env.NODE_ENV === "development" && module.hot) {
+  module.hot.accept("./rustem", () => {
+    console.log(rustemAge)
+  })
+}
---
+++ src/rustem.tsx
@@ -0,0 +1,1 @@
+export const rustemAge = 22
